334770907bbb1d60b03409884a4ded9f1b271a3f
dmagnant <93270186+dmagnant@users.noreply.github.com>
2021-11-16 13:55:07 -0600

    Add files via upload

---
+++ headphones.ps1
@@ -0,0 +1,2 @@
+Import-Module AudioDeviceCmdlets
+Set-AudioDevice -ID "{0.0.0.00000000}.{091ba866-3673-495f-be64-d3fb304901b5}"
---
+++ speakers.ps1
@@ -0,0 +1,2 @@
+Import-Module AudioDeviceCmdlets
+Set-AudioDevice -ID "{0.0.0.00000000}.{fd02113f-a377-4f4c-a376-5d78e4b55c26}"
